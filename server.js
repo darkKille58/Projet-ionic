24d13159574a3e97427c0defe86d6bd0ed478347
Thianoush98 <83791519+Thianoush98@users.noreply.github.com>
2021-11-08 17:56:04 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3c7066f85ad409ce3eb037359bc4ba82'),
+    },
+  },
+});
